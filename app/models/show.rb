a93d797a53972d49059ccf32b33d6eeab92f787b
kjoos22 <kjoos22@gmail.com>
2020-11-29 17:04:07 -0500

    commit tree f636ccf42080df8cf2f97bcb1d71f1d77fbc679d

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,14 @@
 class Show < ActiveRecord::Base
-  
+    has_many :characters
+    has_many :actors, through: :characters
+    belongs_to :network
+
+    def actors_list
+        actors = []
+        self.actors.each do |actor|
+            actors << actor.first_name + " " + actor.last_name
+        end
+        actors
+    end
+
 end
